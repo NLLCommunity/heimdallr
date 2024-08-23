628b36f229cc208e37806256b070980e59f99eb6
Vegard Berg <mail@myrkvi.com>
2024-08-23 23:42:53 +0200

    chore: fix start script

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ if [[ -f "$DB_PATH" ]]; then
   echo "Existing database is $(stat -c %s "${DB_PATH}") bytes"
 else
   echo "Restoring database from replica"
-  litestream restore -if-replica-exists -v "${DB_PATH}"
+  litestream restore -if-replica-exists "${DB_PATH}"
 fi
 
 litestream replicate -exec "${START_COMMAND}"
