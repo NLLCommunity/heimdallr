ad0ef2043e143dc76b9fe0835e5bc2f9a012dd54
Adaline Simonian <adalinesimonian@gmail.com>
2024-08-23 14:31:40 -0700

    chore: add heroku config

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+START_COMMAND=heimdallr
+
+set -e
+set -u
+set -x
+
+litestream version
+echo "DB_REPLICA_URL=${DB_REPLICA_URL}"
+
+readonly DB_PATH='heimdallr.db'
+
+if [[ -f "$DB_PATH" ]]; then
+  echo "Existing database is $(stat -c %s "${DB_PATH}") bytes"
+else
+  echo "Restoring database from replica"
+  litestream restore -if-replica-exists -v "${DB_PATH}"
+fi
+
+litestream replicate -exec "${START_COMMAND}"
