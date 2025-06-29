4b3f9d25b4b170f2477579a1f347522d20af2944
Adaline Simonian <adalinesimonian@pm.me>
2025-06-29 17:18:13 +0000

    refactor: deduplicate ban message in admin embed

--- listeners/ban.go
+++ listeners/ban.go
@@ -54,7 +54,11 @@ func OnMemberBan(e *events.GuildBan) {
 		embed.AddField("Duration", banData.Duration, true)
 	}
 	if banData.Message != "" {
-		embed.AddField("Message", banData.Message, false)
+		if banData.Message == banData.Reason {
+			embed.AddField("Message", "_Sent reason as message, see above._", false)
+		} else {
+			embed.AddField("Message", banData.Message, false)
+		}
 	}
 
 	_, _ = e.Client().Rest().CreateMessage(
