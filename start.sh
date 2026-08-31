7c9f27da0dfa1beeabed0a5e5c3b793a390de0b4
Vegard Berg <vgberg@gmail.com>
2026-08-31 12:56:26 +0200

    chore: Fly.io migration for prod.

--- start.sh
+++ start.sh
@@ -25,7 +25,10 @@ if [ -f "$DB_PATH" ]; then
   echo "Existing database is $(stat -c %s "${DB_PATH}") bytes"
 else
   echo "Restoring database from replica"
-  litestream restore -config litestream.yml -if-replica-exists "${DB_PATH}"
+  litestream restore \
+    -config litestream.yml \
+    -integrity-check quick \
+    "${DB_PATH}"
 fi
 
 litestream replicate -config litestream.yml -exec "${START_COMMAND}"
